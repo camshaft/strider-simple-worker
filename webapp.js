f3574046061a42e9d5259b45a4439c1489d20820
Cameron Bytheway <cameron@nujii.com>
2013-01-13 17:55:42 -0700

    Add log messages

--- webapp.js
+++ webapp.js
@@ -267,11 +267,11 @@ function registerEvents(emitter) {
           var self = this
           deploy = function(ctx, cb) {
             striderMessage("Deploying to Heroku ...")
-            // Deploy to testing first
+            striderMessage("Deploying test environment ...")
             deployHeroku(self.workingDir,
               data.deploy_config.app+"-test", data.deploy_config.privkey, function(err) {
                 if (err) return cb(err);
-                // Deploy to prod after
+                striderMessage("Deploying production environment ...")
                 deployHeroku(self.workingDir,
                   data.deploy_config.app, data.deploy_config.privkey, cb)
               });
